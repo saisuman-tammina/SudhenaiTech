6f4f873c928f911e913f61db66506850f42dfe28
saisuman-tammina <94602275+saisuman-tammina@users.noreply.github.com>
2023-06-26 16:53:23 +0530

    testimonails carousel changed.

--- js/scripts.js
+++ js/scripts.js
@@ -71,4 +71,35 @@ $('body').append('<div id="toTop" class="top-btn"><img src="assets/images/arrow-
 // const element = document.querySelector('.top-colour');
 // element.classList.add('animate__animated', 'animate__flip', 'animate__infinite');
 
-  
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
