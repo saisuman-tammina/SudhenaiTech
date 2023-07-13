61d1a7a2ec316d8868dc35dbffa8917a7fd83472
saisuman-tammina <94602275+saisuman-tammina@users.noreply.github.com>
2023-07-13 18:13:19 +0530

    onscroll effects has been added.

--- js/scripts.js
+++ js/scripts.js
@@ -5,111 +5,112 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0)
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
   $(".content").slice(0, 7).show();
-  $("#loadMore").on("click", function(e){
+  $("#loadMore").on("click", function (e) {
     e.preventDefault();
     $(".content:hidden").slice(0, 4).slideDown();
-    if($(".content:hidden").length == 0) {
+    if ($(".content:hidden").length == 0) {
       $("#loadMore").text("No Content").addClass("noContent");
     }
   });
-  
+
 })
 
 const navbar = document.querySelector('.nav-fixed');
 window.onscroll = () => {
-    if (window.scrollY > 0) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
+  if (window.scrollY > 0) {
+    navbar.classList.add('nav-active');
+  } else {
+    navbar.classList.remove('nav-active');
+  }
 };
 
 $('body').append('<div id="toTop" class="top-btn"><img src="assets/images/arrow-1.svg" style="rotate: -90deg; width: 14px;height: 14px;position: relative;top: -5px;"></div>');
-    $(window).on('scroll',function () {
-        if ($(this).scrollTop() != 0) {
-            $('#toTop').fadeIn();
-        } else {
-            $('#toTop').fadeOut();
-        }
-    }); 
-    $('#toTop').on('click',function(){
-        $("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
-    }); 
+$(window).on('scroll', function () {
+  if ($(this).scrollTop() != 0) {
+    $('#toTop').fadeIn();
+  } else {
+    $('#toTop').fadeOut();
+  }
+});
+$('#toTop').on('click', function () {
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+});
 
- 
 
-   
+
+
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    scrollbar: {
-      el: ".swiper-scrollbar",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+    950: {
+      slidesPerView: 3,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
+  },
+});
 
 // services carousel
 
-  var swiper = new Swiper(".slide-content-services", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    scrollbar: {
-      el: ".swiper-scrollbar",
+var swiper = new Swiper(".slide-content-services", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+    950: {
+      slidesPerView: 4,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 4,
-        },
-    },
-  });
+  },
+});
+
 
